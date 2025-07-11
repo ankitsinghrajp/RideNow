8b6a4f8caf38292ddff3e08cc032527d8ae526ca
ankit singh chouhan <ankitsinghchouhan682@gmail.com>
2025-07-11 11:32:37 +0530

    Register api created

--- Backend/app.js
+++ Backend/app.js
@@ -7,15 +7,20 @@ const cors = require('cors');
 const app = express();
 
 const connectDB = require('./db/db')
+const userRoutes = require('../Backend/routes/user.routes');
+
 
 connectDB();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.get('/',(req,res)=>{
     res.send("This is the backend of flow of RideNow");
 })
+
+app.use('/users',userRoutes);
 
 
 module.exports = app;
--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,18 +1,17 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
 
-    fullName: {
+    fullname: {
         firstname: {
             type:String,
             required: true,
-            minLength: [3,"The firstname must be atleast 3 characters long."],
-            maxLength: [20,"The firstname must be less than or equal to 20 characters."]
+            minLength: [3,"The firstname must be atleast 3 characters long."]
         },
         lastname: {
             type:String,
-            minLength: [3,"The lastname must be atleast 3 characters long."],
-            maxLength: [20,"The lastname must be less than or equal to 20 characters."]
         }
     },
     email: {
@@ -24,9 +23,29 @@ const userSchema = new mongoose.Schema({
     password: {
         type:String,
         required:true,
+        select: false,
     },
     socketId: {
         type: String,
     }
-
 })
+
+userSchema.methods.generateAuthToken = function(){
+    const token = jwt.sign({_id:this._id},process.env.JWT_SECRET);
+    return token;
+}
+
+userSchema.methods.comparePassword = async function(password){
+    return await bcrypt.compare(password,this.password)
+}
+
+userSchema.statics.hashPassword = async function(password){
+   return await bcrypt.hash(password,10);
+}
+
+
+const userModel = mongoose.model('user',userSchema);
+
+module.exports = userModel;
+
+
